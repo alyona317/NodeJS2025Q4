d235ef911aae198417fa5cfa1d5ba7903a1936e8
alyona317 <oschepkova.alyona@yandex.ru>
2025-11-09 17:49:42 +0300

    feat: add file structure and fix eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { "@typescript-eslint": tseslint.plugin },
-    extends: ["js/recommended", ...tseslint.configs.recommended],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { '@typescript-eslint': tseslint.plugin },
+    extends: ['js/recommended', ...tseslint.configs.recommended],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
       globals: globals.node,
-      rules: {
-        semi: ["error", "always"],
-        quotes: ["error", "single"],
-      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
     },
   },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  tseslint.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' },
+  },
 ]);
