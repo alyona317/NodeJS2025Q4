8d3e4a68baba3fdc20798ac75de94eb5f83be8c2
alyona317 <oschepkova.alyona@yandex.ru>
2025-11-09 23:57:24 +0300

    fix: delete comments

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,3 @@
-// Users are stored as objects that have following properties:
-// id — unique identifier (string, uuid) generated on server side
-// username — user's name (string, required)
-// age — user's age (number, required)
-// hobbies — user's hobbies (array of strings or empty array, required)
 export interface User {
   id: string;
   username: string;
