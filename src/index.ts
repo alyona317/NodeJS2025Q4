a8bba94475a92a2aed1547e6790c546cbad06aaf
alyona317 <oschepkova.alyona@yandex.ru>
2025-11-09 18:03:29 +0300

    feat: add sever port

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { config } from 'dotenv';
+import { createServer } from 'http';
+import { handleRequest } from './server.ts';
+
+config();
+
+const PORT = process.env.PORT || 3000;
+
+const server = createServer(handleRequest);
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
